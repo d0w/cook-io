bad31db88aed93889f4a57ab6ccba0562748ab55
PardeshDhakal <pardesh.dhakal1@gmail.com>
2022-11-12 15:37:13 -0500

    api calls

--- cookbookio/src/App.js
+++ cookbookio/src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
 import './App.sass';
+import Listing from './components/LeftBar/listing.js'
+
+
 
 function App() {
-  return (
+
+  return ( 
     <div className="App">
+      <Listing ingredients="eggs,potatos"></Listing> 
    <h1>Placeholder</h1> 
    <h2> Users will enter ingredients here </h2>
+
+   <h2> Users will enter ingredients here </h2>
+   
     </div>
   );
 }
 
 export default App;
+
--- cookbookio/src/components/LeftBar/listing.js
+++ cookbookio/src/components/LeftBar/listing.js
@@ -1,22 +1,44 @@
-const axios = require("axios");
+import axios from "axios";
 
-const options = {
-  method: 'GET',
-  url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients',
-  params: {
-    ingredients: 'apples,flour,sugar',
-    number: '5',
-    ignorePantry: 'true',
-    ranking: '1'
-  },
-  headers: {
-    'X-RapidAPI-Key': '7957cb8127msh973c5a78f7b4a47p17b934jsne0ec1fd84c29',
-    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-  }
-};
+export default function Listing(props){
+     var recipes = Array(5)
+     const options = {
+      method: 'GET',
+      url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients',
+      params: {
+        ingredients: props.ingredients,
+        number: '5',
+        ignorePantry: 'true',
+        ranking: '1'
+      },
+      headers: {
+        'X-RapidAPI-Key': 'd72340653cmsh492c7c86742913dp17b7d4jsn265838768460',
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+      }
+    };
+    axios.request(options).then(function (response) {
+        for(let i = 0; i < 5; i++){
+            //recipes[i] = response.data[i]
+            //console.log(i, recipes[i])
+        }
+        const data = response.data
+        console.log(data)
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+    }).catch(function (error) {
+        console.error(error);
+   
+    });
+
+    return( 
+    <div>
+        {console.log()}
+    <h1> Recipes:  </h1> 
+   <h2> Hi: {} </h2>
+   </div>  
+   )
+
+}
+
+
+
+
